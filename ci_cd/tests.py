448626b5854687e6e8ef3e4d4bd88d9c239775f1
Jekmen1 <jeko.kobakidze@gmail.com>
2024-04-11 22:45:58 +0400

    correct new flows

--- ci_cd/tests.py
+++ ci_cd/tests.py
@@ -3,6 +3,6 @@ from .models import Book
 # Create your tests here.
 class BookModelTest(TestCase):
     def test_book_creation(self):
-        book = Book.objects.create(title="book 1", author="giorgi")
+        book = Book.objects.create(title="Book 1", author="giorgi")
 
-        self.assertEqual(str(book),'book 1')
+        self.assertEqual(book.title,"Book 1")
